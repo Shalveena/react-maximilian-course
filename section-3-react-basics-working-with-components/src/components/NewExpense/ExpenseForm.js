ad3f7edf2dbd0fb845562a884ffe8633deab6618
Shalveena Rohde <shalveena.s.prasad@gmail.com>
2021-10-10 08:49:51 +1100

    multistate vs single state

--- section-3-react-basics-working-with-components/src/components/NewExpense/ExpenseForm.js
+++ section-3-react-basics-working-with-components/src/components/NewExpense/ExpenseForm.js
@@ -2,25 +2,53 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  //Multiple states:
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
+  // Single state:
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
+
   const titleChangeHandler = (e) => {
+    // Multiple states:
     setEnteredTitle(e.target.value);
+    // console.log(e);
     // console.log(enteredTitle);
     // console.log(e.target.value);
+
+    // Single state:
+    // setUserInput((prevState) => {
+    //   // console.log(userInput.enteredTitle);
+    //   return { ...prevState, enteredTitle: e.target.value };
+    // });
   };
 
   const amountChangeHandler = (e) => {
+    // Multiple states:
     setEnteredAmount(e.target.value);
     // console.log(enteredAmount);
     // console.log(e.target.value);
+
+    // Single state:
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: e.target.value };
+    // });
   };
 
   const dateChangeHandler = (e) => {
+    // Multiple states:
     setEnteredDate(e.target.value);
     // console.log(enteredDate);
+
+    // Single state:
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: e.target.value };
+    // });
   };
 
   return (
